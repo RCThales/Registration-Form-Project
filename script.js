2df443fecee37871b18da45479c16239ee25f362
Thales Cardoso <canadathales@gmail.com>
2022-10-01 13:44:55 -0400

    Changed the whole UI for better UX, and optimized the whole code

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-let option = {}
-let peopleObj = {}
+let option = []
+let peopleArray = []
 let idIndex = 0
-let currentPerson = ''
+let currentIndex = 0
 
 class Person {
     name = ''
@@ -11,30 +10,13 @@ class Person {
     zodiac = ''
     personality = ''
 
-    constructor(_name, _age, _SIN, _zodiac) {
+    constructor(_name, _age, _SIN, _zodiac, _personality) {
         this.name = _name
         this.age = _age
         this.SIN = _SIN
         this.zodiac = _zodiac
-       
-        /*
-        const option = {
-            'Áries': 'Maluco',
-            'Touro': 'Esfomeado',
-            'Gêmeos': 'Lerdo',
-            'Câncer': 'Agitado',
-            'Leão': 'Vaidoso',
-            'Virgem': 'Calmo',
-            'Libra': 'Inteligente',
-            'Escorpião': 'Bravo',
-            'Sagitário': 'Explorador',
-            'Capricórnio': 'Aventureiro',
-            'Aquário': 'Concentrado',
-            'Peixes': 'Teimoso'
-        }
-        */
+        this.personality = _personality
 
-       this.personality = option[this.zodiac]
     }
 
 }
@@ -45,43 +27,66 @@ class Person {
 window.addEventListener('load', event => {
     clearInfo()
 
+    //Getting array from localStorage if it exists
+    if(localStorage.getItem('peopleArray') !== null){
+        peopleArray = JSON.parse(localStorage.getItem('peopleArray'))
+
+        //Rendering the localStorage array to the UI if it exists
+        if (peopleArray !== null) {     
+            render(peopleArray)
+        }
+    } 
+  
+    //Fetching the zodiac information to build the select
     fetch('https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/zodiac.json')
         .then((response) => response.json())
         .then((data) => {
-
-           
+  
             /*Getting the key values from the JSON file and also the first element from an array of personality traces*/
-            const zodiac = Object.keys(data.western_zodiac)
-            const keywords = data.western_zodiac.Aquarius.keywords[0]
-            const randomNum = Math.floor(Math.random() * 3)
+            let zodiac = Object.keys(data.western_zodiac)
 
-            //Building object with the zodiac name as key, and one of the 3 personality traces in the keywords array for that zodiac.
-            zodiac.forEach((element, i) => {
-                const personalityWord = data.western_zodiac[zodiac[i]].keywords[randomNum]
-                Object.assign(option, { [zodiac[i]]: personalityWord[0].toUpperCase() + personalityWord.substring(1) })
-            });
-
-            //Building the select dynamically
             let select = document.getElementById("zodiac");
-            for (const key in option) {     
+            //Building object with the zodiac name as key, and one of the 3 personality traces in the keywords array for that zodiac.
+            zodiac.forEach(item => {
                 let optionElement = document.createElement("option");
-                optionElement.text = key
-                select.add(optionElement)       
-            }
+                optionElement.text = item
+                const random = Math.floor(Math.random() * 3)
+                optionElement.value = JSON.stringify({zodiac: item, personality: data.western_zodiac[item].keywords[random]})                
+                select.add(optionElement)  
+
+                //Creating the OPTION array with personalities and zodiac names
+                option.push({
+                    zodiac : JSON.parse(optionElement.value).zodiac,
+                    personality: JSON.parse(optionElement.value).personality
+                })                
+            })
+
     });
 
 })
+
+
+const clearLocalStorage = () => {
+    localStorage.clear()
+}
 
 const saveInfo = () => {
     const name = document.getElementById('name').value
     const age = document.getElementById('age').value
     const SIN = document.getElementById('SIN').value
-    const zodiac = document.getElementById('zodiac').value
-    const person = new Person(name, age, SIN, zodiac)
-
-    //Checks
+    let zodiacObj = {}
+    try {
+        zodiacObj = JSON.parse(document.getElementById('zodiac').value)
+    } catch (e) {
+        console.log('No Zodiac value chosen.')
+    }
+   
+    const person = new Person(name, age, SIN, zodiacObj.zodiac, zodiacObj.personality)
+    //+++++++++++
+    //  Checks
+    //+++++++++++
     if (name.length === 0 || age.length === 0 ||
-        SIN.length === 0 || zodiac.length === 0){
+        SIN.length === 0 || zodiacObj.length === 0){
         alert('Please fill all fields')
         return
     }
@@ -91,29 +96,51 @@ const saveInfo = () => {
         return
     }
 
-    //Getting the current item id
-    const randomId = Math.random()
-
     //Adding person to the literal object of people
-    peopleObj[randomId] = {
-         name: person.name,
-         age: person.age,
-         SIN: person.SIN,
-         zodiac: person.zodiac,
-         personality: person.personality
-         };
+    peopleArray.push({
+        name: person.name,
+        age: person.age,
+        SIN: person.SIN,
+        zodiac: person.zodiac,
+        personality: person.personality
+    })
+    
 
-    const personList = document.getElementById('personList')
-    personList.innerHTML += `<span id="${randomId}">Name: ${person.name}<br>
-    Age: ${person.age}<br>
-    SIN: ${person.SIN}<br>
-    Zodiac: ${person.zodiac}<br>
-    Personality: ${person.personality + " "}<br>
-    <button id="${randomId}" onclick="deletePerson(this)">Delete</button><br>
-    <button id="${randomId}" onclick="editPerson(this)">Edit</button></span><br>`
+    //Render list to the screen
+    render(peopleArray)
+
+    //Saving to local storage
+    saveToLocalStorage("peopleArray", peopleArray)
 
     //Cleaning the fields
     clearInfo()
+    
+}
+
+const saveToLocalStorage = (name ,array) => {
+    localStorage.setItem(name, JSON.stringify(array));
+}
+
+const render = (array) => {
+    let personList = document.getElementById('personList')
+
+    //Resetting the whole innerHtml
+    personList.innerHTML = ''
+
+    let index = 0
+
+    //Building span element
+    array.forEach(element => {
+        personList = document.getElementById('personList')
+        personList.innerHTML += 
+        `<span class="parentSpan" id="${index++}"><strong>Name:</strong> ${element.name}<br>
+        <strong>Age:</strong> ${element.age}<br>
+        <strong>SIN:</strong> ${element.SIN}<br>
+        <strong>Zodiac:</strong> ${element.zodiac}<br>
+        <strong>Personality:</strong> ${element.personality + " "}<br>
+        <button id="btnPeopleDelete" onclick="deletePerson(this)">X</button><br>
+        <button id="btnPeopleEdit" onclick="editPerson(this)">Edit</button></span><br>`
+    });
     
 }
 
@@ -123,66 +150,75 @@ const clearInfo = () => {
     document.getElementById('age').value = ''
     document.getElementById('SIN').value = ''
     document.getElementById('zodiac').value = 'Choose an option'
+    document.getElementById('searchInput').value = '';
 }
 
-const deletePerson = (event) => {
+const deletePerson = (btn) => {
 
-    const id = event.id
-    document.getElementById(id).remove();
-    delete peopleObj[id]
+    const parent = btn.parentElement
+    const index = JSON.parse(localStorage.getItem('peopleArray'))
 
-    alert("Person deleted!");
+    peopleArray.splice(parent.id, 1)
+    saveToLocalStorage('peopleArray', peopleArray)
+
+    //Delete animation
+    parent.classList.add("animationDelete")
+    parent.addEventListener('transitionend', () => {
+        render(peopleArray)
+    })
 
 }
 
-const editPerson = (event) => {
+const editPerson = (btn) => {
+
+    //Clearing elements to rebuild the list
+    let options = document.querySelectorAll('#editOption');
+    options.forEach(option => option.remove());
+
     //getting currentPerson to edit
-    currentPerson = event
+    const parent = btn.parentElement
+    currentIndex = parent.id
 
     document.getElementById("modal").style.display = 'block'
-
-    document.getElementById('nameEdit').value = peopleObj[event.id].name
-    document.getElementById('ageEdit').value = peopleObj[event.id].age
-    document.getElementById('SINEdit').value = peopleObj[event.id].SIN
+    document.getElementById('nameEdit').value = peopleArray[parent.id].name
+    document.getElementById('ageEdit').value = peopleArray[parent.id].age
+    document.getElementById('SINEdit').value = peopleArray[parent.id].SIN
 
     //Building the select dynamically
     let select = document.getElementById("zodiacEdit");
-    for (const key in option) {
-        let optionElement = document.createElement("option");
-        optionElement.text = key
-        select.add(optionElement)
-    }
 
-    document.getElementById('zodiacEdit').value = peopleObj[event.id].zodiac
+    option.forEach((element,index) => {
+        let optionElement = document.createElement("option");
+        optionElement.id = 'editOption'
+        optionElement.text = element.zodiac
+        select.add(optionElement)
+    });
+
+    //*******
+    document.getElementById('zodiacEdit').value = peopleArray[parent.id].zodiac
 
 }
 
 const editConfirm = () => {
 
+    
     //Updating object
-    peopleObj[currentPerson.id].name = document.getElementById('nameEdit').value
-    peopleObj[currentPerson.id].age = document.getElementById('ageEdit').value
-    peopleObj[currentPerson.id].SIN = document.getElementById('SINEdit').value
-    peopleObj[currentPerson.id].zodiac = document.getElementById('zodiacEdit').value
-    peopleObj[currentPerson.id].personality = option[peopleObj[currentPerson.id].zodiac]
+    peopleArray[currentIndex].name = document.getElementById('nameEdit').value
+    peopleArray[currentIndex].age = document.getElementById('ageEdit').value
+    peopleArray[currentIndex].SIN = document.getElementById('SINEdit').value
+    peopleArray[currentIndex].zodiac = document.getElementById('zodiacEdit').value
 
-    let newName = peopleObj[currentPerson.id].name
-    let newAge = peopleObj[currentPerson.id].age
-    let newSIN = peopleObj[currentPerson.id].SIN
-    let newZodiac = peopleObj[currentPerson.id].zodiac
-    let newPersonality = peopleObj[currentPerson.id].personality
+    option.forEach((element, index) => {
+        if (peopleArray[currentIndex].zodiac === element.zodiac){
+            peopleArray[currentIndex].personality = element.personality          
+        }
+    });
 
-      //Updating HTML
-    console.log(document.getElementById(currentPerson.id))
-    document.getElementById(currentPerson.id).outerHTML = `<span id="${currentPerson.id}">Name: ${newName}<br>
-    Age: ${newAge}<br>
-    SIN: ${newSIN}<br>
-    Zodiac: ${newZodiac}<br>
-    Personality: ${newPersonality}<br>
-    <button id="${currentPerson.id}" onclick="deletePerson(this)">Delete</button><br>
-    <button id="${currentPerson.id}" onclick="editPerson(this)">Edit</button></span><br>`
-
+    //Updating HTML
     closeModal()
+
+    render(peopleArray)
+    saveToLocalStorage('peopleArray', peopleArray)
 }
 
 const closeModal = () => {
@@ -190,32 +226,16 @@ const closeModal = () => {
 }
 
 const search = () => {
+    const input = document.getElementById('searchInput');
 
-    if (Object.keys(peopleObj).length > 0) {
-        input = document.getElementById('searchInput');
-        filter = input.value.toUpperCase();
-        list = document.getElementById("personList");
-
-        var regex = new RegExp(filter, "i");
-  
-        console.log(peopleObj)
-
-         for (let index = 0; index < Object.keys(peopleObj).length; index++) {   
-             const name = Object.values(peopleObj).map(e => e.name)[index]
-             const id = Object.keys(peopleObj)[index]
-             
-             if (name.search(regex) === -1){
-                document.getElementById(id).style.display = 'none'
-             }
-             else{
-                 document.getElementById(id).style.display = 'block'
-             }
-                                  
-        };
-
-
+    if(input.value.length === 0){
+        render(peopleArray)
     }
-
+    else{
+        let filteredArray = peopleArray.filter(value => (value.name.toUpperCase()).indexOf((input.value).toUpperCase()) !== -1)
+        render(filteredArray)
+    }
+    
 }
 
 
